f6d7e4c304670d15a89195e36170e0ed3cee0f78
Fndgzv <fndgzv1a@gmail.com>
2023-09-27 14:59:30 -0600

    Farm Bien 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { GraficaComponent } from './pages/grafica/grafica.component';
+import { PagesComponent } from './pages/pages/pages.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+    ProgressComponent,
+    GraficaComponent,
+    PagesComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
